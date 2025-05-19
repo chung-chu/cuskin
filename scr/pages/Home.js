e5e515e1f890f9f0402281e9cec0126e06a6bc8b
Chung Chu <chungptpi00101@gmail.com>
2025-05-19 16:28:26 +0700

    Create Home.js

---
+++ scr/pages/Home.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <section className="card">
+      <h1>Chăm sóc sắc đẹp cùng CUSKIN</h1>
+      <p>
+        Mỹ phẩm thiên nhiên, dịu nhẹ cho làn da Việt – Từ Hàn Quốc đến tay bạn 💖
+      </p>
+    </section>
+  );
+}
