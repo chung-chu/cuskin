0f7301dbf6ed6154e95df7554981f37e3fe2776a
Chung Chu <chungptpi00101@gmail.com>
2025-05-19 16:29:45 +0700

    Create GioHang.js

---
+++ scr/pages/GioHang.js
@@ -0,0 +1,8 @@
+export default function GioHang() {
+  return (
+    <section className="card">
+      <h1>Giỏ hàng</h1>
+      <p>Chức năng giỏ hàng sẽ được cập nhật trong tương lai.</p>
+    </section>
+  );
+}
