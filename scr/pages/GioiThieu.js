d12f261023dea0af12cede28f5d9827fc21a0b6d
Chung Chu <chungptpi00101@gmail.com>
2025-05-19 16:29:22 +0700

    Create GioiThieu.js

---
+++ scr/pages/GioiThieu.js
@@ -0,0 +1,10 @@
+export default function GioiThieu() {
+  return (
+    <section className="card">
+      <h1>Giới thiệu</h1>
+      <p>
+        CUSKIN là thương hiệu mỹ phẩm thiên nhiên từ Hàn Quốc, mang đến giải pháp chăm sóc sắc đẹp an toàn, hiệu quả cho làn da Việt.
+      </p>
+    </section>
+  );
+}
