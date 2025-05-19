005e9a875dcb9544d8bd31a8d26eb0884a95925d
Chung Chu <chungptpi00101@gmail.com>
2025-05-19 16:30:19 +0700

    Create ThanhToan.js

---
+++ scr/pages/ThanhToan.js
@@ -0,0 +1,9 @@
+export default function ThanhToan() {
+  return (
+    <section className="card">
+      <h1>Thanh toán</h1>
+      <p>Vui lòng kiểm tra lại đơn hàng trước khi thanh toán.</p>
+      <button className="btn">Thanh toán ngay</button>
+    </section>
+  );
+}
