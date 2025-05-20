339c3b9f4cf3325548daab99258314e12f3db0a8
Chung Chu <chungptpi00101@gmail.com>
2025-05-20 16:08:00 +0700

    Create Contact.js

---
+++ scr/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section className="section">
+      <h1>Liên hệ</h1>
+      <p>
+        Email: <a href="mailto:info@cuskin.vn">info@cuskin.vn</a><br />
+        Hotline: 0123 456 789<br />
+        Địa chỉ: 123 Đường Mỹ Phẩm, Quận 1, TP.HCM
+      </p>
+    </section>
+  );
+}
