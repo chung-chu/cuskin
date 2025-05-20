26e8a0734ba934f137be8607e0a3d18759f021a9
Chung Chu <chungptpi00101@gmail.com>
2025-05-20 16:09:02 +0700

    Delete scr/pages/ThanhToan.js

--- scr/pages/ThanhToan.js
+++
@@ -1,9 +0,0 @@
-export default function ThanhToan() {
-  return (
-    <section className="card">
-      <h1>Thanh toán</h1>
-      <p>Vui lòng kiểm tra lại đơn hàng trước khi thanh toán.</p>
-      <button className="btn">Thanh toán ngay</button>
-    </section>
-  );
-}
