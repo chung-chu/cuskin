8ae62b8ed3dd2ff374caac5dc9a04168f064850b
Chung Chu <chungptpi00101@gmail.com>
2025-05-20 16:07:38 +0700

    Create About.js

---
+++ scr/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section">
+      <h1>Giới thiệu CUSKIN</h1>
+      <p>CUSKIN là thương hiệu mỹ phẩm Việt Nam với sứ mệnh mang lại làn da khỏe mạnh, rạng rỡ cho mọi người.</p>
+    </section>
+  );
+}
