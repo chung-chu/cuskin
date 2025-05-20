4b87af89311fc312ab53841d30fc5b797a1be013
Chung Chu <chungptpi00101@gmail.com>
2025-05-20 16:07:14 +0700

    Create Products.js

---
+++ scr/pages/Products.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Products() {
+  return (
+    <section className="section">
+      <h1>Tất cả sản phẩm</h1>
+      <p>Bạn có thể thêm danh sách sản phẩm chi tiết tại đây.</p>
+    </section>
+  );
+}
