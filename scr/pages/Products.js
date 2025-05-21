c4e742d8452a006d4fd6e5aef79ff2846b748843
Chung Chu <chungptpi00101@gmail.com>
2025-05-21 08:29:30 +0700

    Delete scr/pages/Products.js

--- scr/pages/Products.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function Products() {
-  return (
-    <div>
-      <h1>Sản phẩm</h1>
-      <p>Đây là trang danh sách sản phẩm của CUSKIN.</p>
-    </div>
-  );
-}
