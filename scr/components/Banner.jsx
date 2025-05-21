0ffe46d04d4a2bc20ee34310081e33051be9730b
Chung Chu <chungptpi00101@gmail.com>
2025-05-21 08:27:37 +0700

    Update Banner.jsx

--- scr/components/Banner.jsx
+++ scr/components/Banner.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import "./Banner.css";
+import bannerImg from "../assets/banner.jpg";
 
+export default function Banner() {
+  return (
+    <section className="cuskin-banner">
+      <img src={bannerImg} alt="Banner" />
+      <div className="banner-content">
+        <h1>Chăm sóc làn da chuẩn Hàn Quốc với Cuskin</h1>
+        <p>Dược mỹ phẩm nhập khẩu, an toàn & hiệu quả.</p>
+      </div>
+    </section>
+  );
+}
