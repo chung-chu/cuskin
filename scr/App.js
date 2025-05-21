79c9fc1d7a6d6b451552defa0f0af5e83d760d5a
Chung Chu <chungptpi00101@gmail.com>
2025-05-21 08:26:49 +0700

    Update App.js

--- scr/App.js
+++ scr/App.js
@@ -1,36 +1,29 @@
 import React from "react";
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <header>
-        <nav className="navbar">
-          <NavLink to="/" end className="logo">
-            <span>CUSKIN</span>
-          </NavLink>
-          <NavLink to="/products">Sản phẩm</NavLink>
-          <NavLink to="/about">Giới thiệu</NavLink>
-          <NavLink to="/contact">Liên hệ</NavLink>
-        </nav>
-      </header>
+    <div className="cuskin-app">
+      <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
-      <footer>
-        <div>© 2025 CUSKIN – Made with 💖 by Chung Chu | Hotline: 0123 456 789</div>
-      </footer>
-    </>
+      <Footer />
+    </div>
   );
 }
 
