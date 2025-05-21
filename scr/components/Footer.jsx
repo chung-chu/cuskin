6afcecc6235eeaa28f342ea1c9eee986d86d5381
Chung Chu <chungptpi00101@gmail.com>
2025-05-21 08:27:24 +0700

    Update Footer.jsx

--- scr/components/Footer.jsx
+++ scr/components/Footer.jsx
@@ -1,1 +1,13 @@
+import React from "react";
+import "./Footer.css";
 
+export default function Footer() {
+  return (
+    <footer className="cuskin-footer">
+      <div className="container">
+        <div>© {new Date().getFullYear()} CUSKIN – Made with 💖 by Chung Chu</div>
+        <div>Hotline: 0123 456 789 | Địa chỉ: 18 Phổ Quang, Q.Tân Bình, TP.HCM</div>
+      </div>
+    </footer>
+  );
+}
