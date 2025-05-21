6928e2fb2fad2a1e275394f0f7383683d192175e
Chung Chu <chungptpi00101@gmail.com>
2025-05-21 08:27:07 +0700

    Update Header.jsx

--- scr/components/Header.jsx
+++ scr/components/Header.jsx
@@ -1,1 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
+import "./Header.css";
 
+export default function Header() {
+  return (
+    <header className="cuskin-header">
+      <div className="container">
+        <NavLink to="/" className="logo">
+          <img src={logo} alt="CUSKIN logo" height={40} />
+          <span>CUSKIN VIỆT NAM</span>
+        </NavLink>
+        <nav>
+          <NavLink to="/products" activeclassname="active">Sản phẩm</NavLink>
+          <NavLink to="/about" activeclassname="active">Giới thiệu</NavLink>
+          <NavLink to="/contact" activeclassname="active">Liên hệ</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+}
