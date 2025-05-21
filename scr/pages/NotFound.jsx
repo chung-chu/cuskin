b1e29d7c76981280dc9a40c24a930cff22af4a98
Chung Chu <chungptpi00101@gmail.com>
2025-05-21 08:31:00 +0700

    Create NotFound.jsx

---
+++ scr/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="section">
+      <h1>404</h1>
+      <p>Trang bạn tìm kiếm không tồn tại.</p>
+    </div>
+  );
+}
