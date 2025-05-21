11971d4fa97db0b877471226bf00c0d98a2b4944
Chung Chu <chungptpi00101@gmail.com>
2025-05-21 08:29:50 +0700

    Create About.jsx

---
+++ scr/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section">
+      <h2>Giới thiệu về CUSKIN</h2>
+      <p>
+        CUSKIN là đối tác chiến lược của tập đoàn mỹ phẩm y tế chuyên nghiệp Hàn Quốc, mang đến các sản phẩm chăm sóc da chất lượng cao cho người Việt.
+      </p>
+      <ul>
+        <li>Thương hiệu Hàn Quốc chính hãng</li>
+        <li>Dược mỹ phẩm an toàn, lành tính</li>
+        <li>Dịch vụ chăm sóc khách hàng tận tâm</li>
+      </ul>
+    </section>
+  );
+}
