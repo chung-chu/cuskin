06924e560dde2853a132f1baca180b453175be93
Chung Chu <chungptpi00101@gmail.com>
2025-05-21 08:28:03 +0700

    Update ProductList.jsx

--- scr/components/ProductList.jsx
+++ scr/components/ProductList.jsx
@@ -1,1 +1,11 @@
+import React from "react";
+import ProductCard from "./ProductCard";
+import "./ProductList.css";
 
+export default function ProductList({ products }) {
+  return (
+    <div className="product-list">
+      {products.map(prod => <ProductCard key={prod.id} product={prod} />)}
+    </div>
+  );
+}
