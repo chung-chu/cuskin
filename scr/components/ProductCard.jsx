c880aa5ed381f94bbee3ca718fa73f9631c391b3
Chung Chu <chungptpi00101@gmail.com>
2025-05-21 08:27:49 +0700

    Update ProductCard.jsx

--- scr/components/ProductCard.jsx
+++ scr/components/ProductCard.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import "./ProductCard.css";
 
+export default function ProductCard({ product }) {
+  return (
+    <div className="product-card">
+      <img src={product.image} alt={product.name} />
+      <div className="product-info">
+        <h3>{product.name}</h3>
+        <p>{product.shortDesc}</p>
+        <button>Xem chi tiết</button>
+      </div>
+    </div>
+  );
+}
